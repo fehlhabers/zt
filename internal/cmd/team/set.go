dabe0518c9aabb3ad9e450c55acae6de0458665d
Kaj Fehlhaber <kaj.fehlhaber@gmail.com>
2024-12-05 17:54:19 +0100

    zt handover

--- internal/cmd/team/set.go
+++ internal/cmd/team/set.go
@@ -2,20 +2,21 @@ package team
 
 import (
 	"github.com/charmbracelet/log"
+	"github.com/fehlhabers/zt/internal/core/config"
 	"github.com/spf13/cobra"
 )
 
-func NewSet() *cobra.Command {
+func NewSwitch() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "set <team>",
-		Short: "Sets your team",
+		Use:   "switch <team>",
+		Short: "Switch your team",
 		Long:  `Set the team which is used to identify your timer among others`,
 		Run: func(cmd *cobra.Command, args []string) {
 
 			if len(args) != 1 {
 				log.Fatal("Command requires 'team' to be included as argument")
 			}
-
+			config.SwitchTeam(args[0])
 		},
 	}
 
